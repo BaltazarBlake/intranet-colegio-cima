5789802318d7d82868ce33052f50567c9e3ba06b
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2018-05-03 00:10:52 -0500

    Navigation template

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -7,31 +7,31 @@
       a(href="?view=logout" @click="deleteData").btn--danger.is-active.navigation__btn Cerrar Sesión
     ul.row
       li.navigation__item.col-xs-12
-        a(@click="student").link
+        a(@click="student").navigation__link
           span.icon-home
           span Inicio
       li.navigation__item.col-xs-12
-        a(@click="score").link
+        a(@click="score").navigation__link
           span.icon-file
           span Exámenes
       li.navigation__item.col-xs-12
-        a(@click="assistance").link
+        a(@click="assistance").navigation__link
           span.icon-calendar-1
           span Asistencia
       li.navigation__item.col-xs-12
-        a(@click="horary").link
+        a(@click="horary").navigation__link
           span.icon-horary
           span Horario
       li.navigation__item.col-xs-12
-        a(@click="event").link
+        a(@click="event").navigation__link
           span.icon-notes
           span Observaciones
       li.navigation__item.col-xs-12
-        a(@click="teacher").link
+        a(@click="teacher").navigation__link
           span.icon-team
           span Profesores
       li.navigation__item.col-xs-12
-        a(@click="regulation").link
+        a(@click="regulation").navigation__link
           span.icon-book
           span Reglamento Int.
 </template>
