8c0641e9592922714b9ad41c799e7a074650d117
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2018-07-04 10:00:51 -0500

    pendiente head fixen en table

--- src/components/CoursesExams.vue
+++ src/components/CoursesExams.vue
@@ -111,32 +111,54 @@
       template(slot='body')
         .row
           .col-xs-12
-            .table
-              .table-header
-                table
-                  thead
+            // .table
+            //   .table-header
+            //     table
+            //       thead
+            //         tr
+            //           th(rowspan="2", colspan="2") ÁREA CURRICULAR
+            //           th(colspan="8" v-if="reportPrint") BIMESTRE
+            //           th(rowspan="2", colspan="2") PROM. ACUM.
+            //         tr
+            //           th(colspan="2") I
+            //           th(colspan="2") II
+            //           th(colspan="2") III
+            //           th(colspan="2") IV
+            //   .table-content
+            //     table
+            //       tbody
+            //         template(v-for="data in reportPrint")
+            //           td.u-gray(colspan="12" v-if="reportPrint") {{data.area}}
+            //           template(v-for="c in data.cursos")
+            //             tr
+            //               td(colspan="2") {{c.curso}}
+            //               template(v-for="nota in c.notas")
+            //                 td(colspan="2") {{nota.nota}}
+            //               template(v-for="n in (4- c.notas.length)")
+            //                 td(colspan="2") -
+            //               td.u-mla(colspan="2") {{c.promedio}}
+            table.header-fixed
+              thead
+                tr
+                  th(rowspan="2", colspan="2") ÁREA CURRICULAR
+                  th(colspan="8" v-if="reportPrint") BIMESTRE
+                  th(rowspan="2", colspan="2") PROM. ACUM.
+                tr
+                  th(colspan="2") I
+                  th(colspan="2") II
+                  th(colspan="2") III
+                  th(colspan="2") IV
+              tbody
+                template(v-for="data in reportPrint")
+                  td.u-gray(colspan="12" v-if="reportPrint") {{data.area}}
+                  template(v-for="c in data.cursos")
                     tr
-                      th(rowspan="2") ÁREA CURRICULAR
-                      th(colspan="8" v-if="reportPrint") BIMESTRE
-                      th(rowspan="2", colspan="2") PROM. ACUM.
-                    tr
-                      th(colspan="2") I
-                      th(colspan="2") II
-                      th(colspan="2") III
-                      th(colspan="2") IV
-              .table-content
-                table
-                  tbody
-                    template(v-for="data in reportPrint")
-                      td.u-gray(colspan="12" v-if="reportPrint") {{data.area}}
-                      template(v-for="c in data.cursos")
-                        tr
-                          td(colspan="2") {{c.curso}}
-                          template(v-for="nota in c.notas")
-                            td(colspan="2") {{nota.nota}}
-                          template(v-for="n in (4- c.notas.length)")
-                            td(colspan="2") -
-                          td.u-mla(colspan="2") {{c.promedio}}
+                      td(colspan="2") {{c.curso}}
+                      template(v-for="nota in c.notas")
+                        td(colspan="2") {{nota.nota}}
+                      template(v-for="n in (4- c.notas.length)")
+                        td(colspan="2") -
+                      td.u-mla(colspan="2") {{c.promedio}}
                     
 
 </template>
