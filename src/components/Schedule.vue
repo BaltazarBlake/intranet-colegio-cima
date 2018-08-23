ed476a3dfb8539a2c094080969c73b2ff39d4002
David Supo <davidjsupoo@gmail.com>
2018-08-23 09:29:57 -0500

    Refactorizando horario

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -11,7 +11,7 @@
                 button(@click="print('print')").btn--warning.is-active
                   span.icon-printer
                   span Imprimir
-    template(v-if="report")
+    template(v-if="report != null && !report.err")
         .container.target
           .row.main-center
             template(v-if="report == 1")
@@ -25,199 +25,118 @@
                   .horary__header
                     .horary__cell(:style="'width:'+width_dias+'%'")
                       strong.horary__cell-title Hora
-                    .horary__cell(:style="'width:'+width_dias+'%'", v-for='dia in report.array_dias')
-                      strong.horary__cell-title {{dia.nombre_dia}}
+                    .horary__cell(:style="'width:'+width_dias+'%'", v-for='dia in report')
+                      strong.horary__cell-title {{dia.dia}}
                   .horary__body
                     div.column-w
                       .horary__hours(:style="'height:'+heigth_horas+'%'", v-for='(hora, i) in report.horas')
-                        span {{hora}}
-                    .horary__column(v-for='dia in report.array_dias')
+                        span {{hora.inicio}}
+                    .horary__column(v-for='dia in report')
                       article.day
                         .day__title(:id="'dia-hr'+dia.numero_dia")
-                          h1.title2 {{dia.nombre_dia}}
-                        div.horary__column--desktop(v-html='drawCurse(dia.array_horas)')
+                          h1.title2 {{dia.dia}}
+                        div.horary__column--desktop
+                          .course--desktop(:style="'height:'+heigth_horas * curso.cantidad_horas+'%'" v-for='curso in dia.cursos')
+                            .texto-curso(style='')
+                              span.horary__data--desktop.font-size-regular {{curso.asignatura}}
+                              span.horary__data--desktop.font-size-small {{curso.docente}}
+                              .horary--movile
+                                .col-xs-12
+                                  .course-horary.row
+                                    .col-xs-12
+                                      p.title2 {{curso.asignatura}}
+                                    .col-xs-12.col-xm-6
+                                      strong.normal Hora Inicio: 
+                                      strong.normal {{curso.horas[0].inicio}}
+                                    .col-xs-12.col-xm-6
+                                      strong.normal Hora Fin: 
+                                      strong.normal {{curso.horas[curso.horas.length-1].fin}}
+    template(v-else-if="report && report.err")
+      .container.target
+        .col-xs-12
+          .card
+            .row
+              .col-xs-12
+                h1.font-size-xxx-large.text--center El horario no se encuentra.
     template(v-else)
       .m-t-s
         spinner
 </template>
 
 <script>
-import Spinner from './global/Spinner';
-import {getSchedule} from '../functions/fetchFunctions';
+import Spinner from "./global/Spinner";
+import { getSchedule } from "../functions/fetchFunctions";
 
 export default {
-  components:{Spinner},
-  data(){
-    return{
-      report:null,
+  components: { Spinner },
+  data() {
+    return {
+      report: null,
       heigth_horas: 0,
       width_dias: 0,
       errors: null,
-      alumno:null,
-      id:[],
+      alumno: null,
+      id: [],
       colors: [
-        'EF9A9A',
-        'D7CCC8',
-        'B2EBF2',
-        'FFF9C4',
-        'E0E0E0',
-        '78909C',
-        'A5D6A7',
-        'C5CAE9',
-        'FFCC80',
-        'E1BEE7',
-        'F0F4C3',
-        'C8E6C9',
-        'FFCCBC',
-        'F48FB1',
-        'F8BBD0',
-        'FDD835',
-        'FFCDD2',
-        'E3F2FD',
-        'FFE0B2',
-        'FFEBEE',
-        ],
-    }
+        "EF9A9A",
+        "D7CCC8",
+        "B2EBF2",
+        "FFF9C4",
+        "E0E0E0",
+        "78909C",
+        "A5D6A7",
+        "C5CAE9",
+        "FFCC80",
+        "E1BEE7",
+        "F0F4C3",
+        "C8E6C9",
+        "FFCCBC",
+        "F48FB1",
+        "F8BBD0",
+        "FDD835",
+        "FFCDD2",
+        "E3F2FD",
+        "FFE0B2",
+        "FFEBEE"
+      ]
+    };
   },
-  async mounted(){
+  async mounted() {
     await this.getData();
   },
-  methods:{
-    async getData(){
+  methods: {
+    async getData() {
       let res;
-      this.alumno = JSON.parse(localStorage.getItem('cima-estudiante'));
-      if(!localStorage.getItem('cima-estudiante-horario')){
+      this.alumno = JSON.parse(localStorage.getItem("cima-estudiante"));
+      if (!localStorage.getItem("cima-estudiante-horario")) {
         res = await getSchedule(this.alumno.idalumnocolegio);
-        res = res.data;
-        localStorage.setItem('cima-estudiante-horario',JSON.stringify(res));
-      }else{
-        res = JSON.parse(localStorage.getItem('cima-estudiante-horario'));
+        res = res.data ? res.data : res;
+        if (res.data)
+          localStorage.setItem("cima-estudiante-horario", JSON.stringify(res));
+      } else {
+        res = JSON.parse(localStorage.getItem("cima-estudiante-horario"));
       }
       this.report = res;
-
-      console.log(this.report);
       let horas = [];
-      this.report[0].cursos.map(curso=>{
-        curso.horas.map(hora=>{
+      if (!res.err) {
+        console.log('asdasdasdasdasd');
+        this.report[0].cursos.map(curso => {
+          curso.horas.map(hora => {
             horas.push(hora);
-        })
-      })
-      this.report.horas = horas;
-      console.log(horas);
-      this.heigth_horas = 100 / this.report.horas.length;
-      this.width_dias = 100 / this.report.length;
-    },
-    drawCurse(array_horas) {
-      console.log('-----')
-      let tmp_curso = "x";
-      let hora_inicio;
-      let hora_fin;
-      let profe = "x";
-      let id_profe;
-      let tmp_id = "id";
-      let size = 0;
-      let render_curso = "";
-      let color;
-      let hoursDay = this.report.horas;
-      let courseDay = [];
-      let rgba; 
-      
-
-      for (let i = 0; i < array_horas.length; i++) {
-        let curso = array_horas[i].curso;
-        let id_curso = array_horas[i].id_curso;
-
-        
-
-        /*Color a RGBA*/
-        let r,g,b,rgba;
-        /*Color a RGBA*/
-
-
-        if (tmp_curso != curso) {
-          if (tmp_curso != "x") {            
-            render_curso += `
-            <div style='height:${this.heigth_horas * size / 2}%;background-color:#${color}' class='course--desktop'>
-              <div class='texto-curso' style=''>
-                <span class='horary__data--desktop font-size-regular'>${tmp_curso}</span>
-                <span class='horary__data--desktop font-size-small'>${profe}</span>
-                <div class='horary--movile'>
-                  <div class='col-xs-12'>
-                    <div class='course-horary row'>
-                      <div class="col-xs-12">
-                        <p class="title2">${ tmp_curso == " " ? '' : tmp_curso}</p>
-                      </div>
-                      <div class='col-xs-12 col-xm-6'>
-                        <strong class='normal'>Hora Inicio:</strong>
-                        <strong class='normal'>${hora_inicio}</strong>
-                      </div>
-                      <div class='col-xs-12 col-xm-6'>
-                        <strong class='normal'>Hora Fin:</strong>
-                        <strong class='normal'>${hora_fin}</strong>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          `;
-          }
-          //comienza nuevo curso
-          tmp_curso = curso;          
-          hora_inicio = array_horas[i].hora_inicio;
-          hora_fin = array_horas[i].hora_fin;
-          profe = array_horas[i].profesor;
-          id_profe = array_horas[i].idprofesor;
-
-          tmp_id = array_horas[i].id_curso;
-          // console.log(tmp_curso,' - ',tmp_id);
-          let i_id = this.id.indexOf(id_curso);
-          if(i_id===-1){
-            this.id.push(id_curso);
-          }
-          color = this.colors[this.id.indexOf(id_curso)];
-
-
-          size = 1;
-        } else {
-          hora_fin = array_horas[i].hora_fin;
-          // mismo curso
-          size++;
-        }
+          });
+        });
+        this.report.horas = horas;
+        console.log(horas);
+        this.heigth_horas = 100 / this.report.horas.length;
+        this.width_dias = 100 / this.report.length;
       }
-      render_curso += `
-        <div style='height:${this.heigth_horas * size / 2}%;background-color:#${color.toString(16)}' class='course--desktop'>
-          <div class='texto-curso' style=''>
-            <span class='horary__data--desktop font-size-regular'>${tmp_curso}</span>
-            <span class='horary__data--desktop font-size-small'>${profe}</span>
-            <div class='horary--movile'>
-              <div class='col-xs-12'>
-                <div class='course-horary row'>
-                  <div class="col-xs-12">
-                    <p class="title2">${ tmp_curso == " " ? '' : tmp_curso}</p>
-                  </div>
-                  <div class='col-xs-12 col-xm-6'>
-                    <strong class='normal'>Hora Inicio:</strong>
-                    <strong class='normal'>${hora_inicio}</strong>
-                  </div>
-                  <div class='col-xs-12 col-xm-6'>
-                    <strong class='normal'>Hora Fin:</strong>
-                    <strong class='normal'>${hora_fin}</strong>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      `;
-      return render_curso;
     },
-     print(idReport) {
+    print(idReport) {
       let report = document.getElementById(idReport);
       let style = document.styleSheets;
 
       let rules = [];
-      let styleString = '';
+      let styleString = "";
 
       for (let i = 0; i < style.length; i++) {
         rules.push(style[i].cssRules);
@@ -230,7 +149,9 @@ export default {
         "<!DOCTYPE html>" +
         "<html lang='en'>" +
         "<head>" +
-        "<style>"+ styleString +"</style>" +
+        "<style>" +
+        styleString +
+        "</style>" +
         "<meta charset='UTF-8'>" +
         "<meta name='viewport' content='width=device-width, initial-scale=1.0'>" +
         "<title>Reporte</title>" +
@@ -244,6 +165,6 @@ export default {
       printWindow.document.write(print);
       printWindow.document.close();
     }
-  }  
-}
+  }
+};
 </script>
